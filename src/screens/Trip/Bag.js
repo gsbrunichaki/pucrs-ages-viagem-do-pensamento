9fc0c3cf617332a74fe0455f5a9240a32af370d2
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-05-26 15:07:15 -0300

    fix: ajustes nos tamanhos e espaçamentos dos componentes

--- src/screens/Trip/Bag.js
+++ src/screens/Trip/Bag.js
@@ -1,53 +1,87 @@
 import React, { useState } from "react";
-import { View, ImageBackground, StyleSheet,ScrollView} from "react-native";
+import { View, ImageBackground, StyleSheet, ScrollView } from "react-native";
 import CloudImageBackground from "../../components/CloudImageBackground";
 import PageBanner from "../../components/PageBanner";
-import { Text, Button,Item,Input } from "native-base";
-import { white } from "react-native-paper/lib/typescript/src/styles/colors";
+import { Text, Button, Item, Input } from "native-base";
 
 export default function Bag({ route, navigation }) {
-  const [pensamentos,setPensamentos] = useState(""); 
   const { aircraft } = route.params;
+
+  const [thought, setThought] = useState("");
+
   return (
     <CloudImageBackground>
-       <Text> {aircraft} </Text>
-       <PageBanner title={'Que pensamentos você levará na mala?'} />
-      
-        
-        <ImageBackground  style={styles.camiseta} resizeMode="contain" source={require("../../assets/shirt.png")}>
-        
-        <ScrollView style={[styles.scrollview]}>
-          <Text style={styles.text}> {pensamentos} </Text>
-        </ScrollView>
+      <Text> {aircraft} </Text>
+      <PageBanner title={"Que pensamentos você levará na mala?"} />
+      <View style={{ alignItems: "center" }}>
+        <ImageBackground
+          style={styles.shirt}
+          resizeMode="contain"
+          source={require("../../assets/shirt.png")}
+        >
+          <ScrollView>
+            <Text style={styles.text}> {thought} </Text>
+          </ScrollView>
+        </ImageBackground>
+      </View>
+      <Item style={styles.inputSpacing}>
+        <Input
+          value={thought}
+          placeholder="Pensamento"
+          style={styles.input}
+          onChangeText={(value) => {
+            setThought(value);
+          }}
+        />
+      </Item>
 
-        </ImageBackground>    
-        <Item style={styles.inputSpacing}>
-          <Input 
-            value={pensamentos}
-            placeholder="Pensamento" 
-            style={styles.input}
-            onChangeText={(value)=>{setPensamentos(value)}}
-           />
-        </Item>
-
-      <Button style={[styles.button,styles.button1]} full rounded onPress={()=>{setPensamentos("Nossa, legal ")}}>
+      <Button
+        style={[styles.button, styles.button1]}
+        full
+        rounded
+        onPress={() => {
+          setThought("Nossa, legal ");
+        }}
+      >
         <Text>Nossa, legal</Text>
       </Button>
 
-      <Button style={[styles.button,styles.button2]} full rounded onPress={()=>{setPensamentos("Humm, Ok ")}}>
+      <Button
+        style={[styles.button, styles.button2]}
+        full
+        rounded
+        onPress={() => {
+          setThought("Humm, Ok ");
+        }}
+      >
         <Text>Humm, Ok.</Text>
       </Button>
-      
-      
-      <Button style={[styles.button,styles.button3]} full rounded onPress={()=>{setPensamentos("Ah, não gostei ")}}>
+
+      <Button
+        style={[styles.button, styles.button3]}
+        full
+        rounded
+        onPress={() => {
+          setThought("Ah, não gostei ");
+        }}
+      >
         <Text>Ah, não gostei...</Text>
       </Button>
 
-      <Button style={[styles.button,styles.button4]} full rounded onPress={_ => { navigation.navigate("TripIslands", { aircraft, thoughts: [pensamentos, "t2", "t3"] }) }}>
+      <Button
+        style={[styles.button, styles.button4]}
+        full
+        rounded
+        onPress={(_) => {
+          navigation.navigate("TripIslands", {
+            aircraft,
+            thoughts: [thought, "t2", "t3"],
+          });
+        }}
+      >
         <Text>Continuar</Text>
       </Button>
     </CloudImageBackground>
-
   );
 }
 
@@ -59,6 +93,8 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   text: {
+    textAlign: "center",
+    fontWeight: "bold",
     color: "white",
   },
   button: {
@@ -67,86 +103,80 @@ const styles = StyleSheet.create({
   },
 
   button1: {
-    backgroundColor:"#13C4A3",
-
+    backgroundColor: "#13C4A3",
   },
   button2: {
-    backgroundColor:"#F1C30F",
-
+    backgroundColor: "#F1C30F",
   },
   button3: {
-    backgroundColor:"#F75C03",
-
+    backgroundColor: "#F75C03",
   },
-  button4:{
+  button4: {
     marginHorizontal: 80,
   },
   inputSpacing: {
     backgroundColor: "#FFF",
-    marginBottom: 20,
     borderBottomWidth: 0,
-    width:"100%",
     borderRadius: 10,
     borderColor: "#000",
-    elevation: 10,
+    elevation: 3,
     marginLeft: 30,
     marginRight: 30,
     marginTop: 10,
+    marginBottom: 20,
   },
   input: {
     width: "100%",
     color: "#000",
   },
-  camiseta: {
-    flex : 1,
-    paddingTop: 110,
-    paddingLeft: 90,
+  shirt: {
+    width: 256,
+    height: 256,
+    marginTop: 10,
+    paddingTop: 50,
+    paddingBottom: 30,
+    paddingHorizontal: 70,
   },
   content: {
     flex: 1,
-    marginTop: '1%',
+    marginTop: "1%",
     alignItems: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   contentImage: {
     flex: 1,
     alignContent: "center",
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     flexDirection: "row",
     // boar
-
   },
   Emotion: {
-
     flex: 0.43,
     width: "75%",
     height: "75%",
-    resizeMode: 'contain',
+    resizeMode: "contain",
     alignSelf: "center",
-
   },
   Bagimage: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     width: "90%",
     height: "90%",
-    alignSelf: "center"
+    alignSelf: "center",
   },
   Boardimage: {
     flex: 1,
     width: "100%",
     height: "90%",
-    resizeMode: 'contain',
-    alignSelf: "center"
-
-
+    resizeMode: "contain",
+    alignSelf: "center",
   },
   title: {
     color: "#FFFFFF",
     fontSize: 17,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 1,
-    textAlign: "center"
+    textAlign: "center",
   },
 });
