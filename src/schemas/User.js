db2be8df5e34b55c635beccd23a2bf5d98b234b6
andremarques2911 <andregfpa@gmail.com>
2020-06-10 00:13:09 -0300

    Persistir na hora do cadastro do usuário o gênero da criança no firebase.

--- src/schemas/User.js
+++ src/schemas/User.js
@@ -1,11 +1,13 @@
-import Schema from "../lib/schema"
-import DateLib from "../lib/date"
+import Schema from "../lib/schema";
+import DateLib from "../lib/date";
+import Genders from "../enums/Genders";
 
 export default new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     city: { type: String, required: true },
     childrenName: { type: String, required: true },
+    childrenGender: { type: Genders, required: true },
     childrenBirthday: { type: String, required: true, set: (value) => {
         return DateLib.dmY2Ymd(DateLib.formatDate(value))
     } },
