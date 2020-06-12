c45c1efd480f8f96af5c350dae22ed1c2811015b
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-06-12 17:18:46 -0300

    esboço do modal de tutorial

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { AppLoading } from 'expo';
-import { StyleProvider } from 'native-base';
-import getTheme from './native-base-theme/components';
-import commonColor from './native-base-theme/variables/commonColor';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from "react";
+import { AppLoading } from "expo";
+import { StyleProvider } from "native-base";
+import getTheme from "./native-base-theme/components";
+import commonColor from "./native-base-theme/variables/commonColor";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Colors from "./src/assets/Colors/Colors";
 
 import UserService from "./src/services/User";
 
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-import Home from './src/screens/Home';
-import TripAircraft from './src/screens/Trip/Aircraft';
-import TripBag from './src/screens/Trip/Bag';
-import TripIslands from './src/screens/Trip/Islands';
-import TripReactions from './src/screens/Trip/Reactions';
-import TripFeedback from './src/screens/Trip/Feedback';
-import Colors from "./src/assets/Colors/Colors";
-import Perfil from './src/screens/Passport/Perfil';
-import Historico from './src/screens/Passport/Historico';
-import Passaporte from './src/screens/Passport/Passaporte';
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import Home from "./src/screens/Home";
+import TripAircraft from "./src/screens/Trip/Aircraft";
+import TripBag from "./src/screens/Trip/Bag";
+import TripIslands from "./src/screens/Trip/Islands";
+import TripReactions from "./src/screens/Trip/Reactions";
+import TripFeedback from "./src/screens/Trip/Feedback";
+import Perfil from "./src/screens/Passport/Perfil";
+import Historico from "./src/screens/Passport/Historico";
+import Passaporte from "./src/screens/Passport/Passaporte";
+import TutorialAircraft from "./src/screens/Tutorial/Aircraft";
+import TutorialBag from "./src/screens/Tutorial/Bag";
+import TutorialIslands from "./src/screens/Tutorial/Islands";
+import TutorialReactions from "./src/screens/Tutorial/Reactions";
+import TutorialFeedback from "./src/screens/Tutorial/Feedback";
 
 console.disableYellowBox = true;
 
@@ -35,8 +40,8 @@ export default function App() {
   useEffect(() => {
     (async () => {
       await Font.loadAsync({
-        Roboto: require('native-base/Fonts/Roboto.ttf'),
-        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
         ...Ionicons.font,
       });
 
@@ -46,32 +51,89 @@ export default function App() {
 
   const anonymousRoutes = (setLogged) => [
     <Screen name="Login" options={{ headerShown: false }}>
-      {props => <Login {...props} setLogged={setLogged} />}
+      {(props) => <Login {...props} setLogged={setLogged} />}
     </Screen>,
     <Screen
       name="Register"
       component={Register}
       options={{
-        title: 'Cadastro',
+        title: "Cadastro",
       }}
-    />
+    />,
   ];
 
   const authenticatedRoutes = (setLogged) => [
     <Screen name="Home">
-      {props => <Home {...props} setLogged={setLogged} />}
+      {(props) => <Home {...props} setLogged={setLogged} />}
     </Screen>,
-    
-    <Screen name="TripAircraft" component={TripAircraft} options={{ title: 'Viagem' }} />,
-    <Screen name="TripBag" component={TripBag} options={{ title: 'Mala de Pensamentos' }} />,
-    <Screen name="TripIslands" component={TripIslands} options={{ title: 'Ilha das Emoções' }} />,
-    <Screen name="TripReactions" component={TripReactions} options={{ title: 'Reações e Comportamento' }} />,
-    <Screen name="TripFeedback" component={TripFeedback} options={{ title: 'Feedback', headerLeft: null }} />,
-    <Screen name="Perfil" component={Perfil} options={{ title: 'Perfil do usuário' }} />,
-    <Screen name="Historico" component={Historico} options={{ title: 'Histórico de viagens' }} />,
-    <Screen name="Passaporte" component={Passaporte} options={{ title: 'Passaporte' }} />,
+
+    <Screen
+      name="TripAircraft"
+      component={TripAircraft}
+      options={{ title: "Viagem" }}
+    />,
+    <Screen
+      name="TripBag"
+      component={TripBag}
+      options={{ title: "Mala de Pensamentos" }}
+    />,
+    <Screen
+      name="TripIslands"
+      component={TripIslands}
+      options={{ title: "Ilha das Emoções" }}
+    />,
+    <Screen
+      name="TripReactions"
+      component={TripReactions}
+      options={{ title: "Reações e Comportamento" }}
+    />,
+    <Screen
+      name="TripFeedback"
+      component={TripFeedback}
+      options={{ title: "Feedback", headerLeft: null }}
+    />,
+    <Screen
+      name="Perfil"
+      component={Perfil}
+      options={{ title: "Perfil do usuário" }}
+    />,
+    <Screen
+      name="Historico"
+      component={Historico}
+      options={{ title: "Histórico de viagens" }}
+    />,
+    <Screen
+      name="Passaporte"
+      component={Passaporte}
+      options={{ title: "Passaporte" }}
+    />,
+    <Screen
+      name="TutorialAircraft"
+      component={TutorialAircraft}
+      options={{ title: "Viagem" }}
+    />,
+    <Screen
+      name="TutorialBag"
+      component={TutorialBag}
+      options={{ title: "Mala de Pensamentos" }}
+    />,
+    <Screen
+      name="TutorialIslands"
+      component={TutorialIslands}
+      options={{ title: "Ilha das Emoções" }}
+    />,
+    <Screen
+      name="TutorialReactions"
+      component={TutorialReactions}
+      options={{ title: "Reações e Comportamento" }}
+    />,
+    <Screen
+      name="TutorialFeedback"
+      component={TutorialFeedback}
+      options={{ title: "Feedback", headerLeft: null }}
+    />,
   ];
-  
+
   if (!isFontLoaded) {
     return <AppLoading />;
   }
@@ -79,16 +141,17 @@ export default function App() {
   return (
     <StyleProvider style={getTheme(commonColor)}>
       <NavigationContainer>
-        <Navigator screenOptions={{
-          headerStyle: {
-            backgroundColor: '#2C73D5'
-          },
-          headerTintColor: Colors.white,
-        }}>
+        <Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#2C73D5",
+            },
+            headerTintColor: Colors.white,
+          }}
+        >
           {isLogged
             ? authenticatedRoutes(setLogged)
-            : anonymousRoutes(setLogged)
-          }
+            : anonymousRoutes(setLogged)}
         </Navigator>
       </NavigationContainer>
     </StyleProvider>
--- src/screens/Tutorial/Aircraft.js
+++ src/screens/Tutorial/Aircraft.js
@@ -1,56 +1,113 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
+import { Body, Button, Card, CardItem } from "native-base";
+import Modal from "react-native-modal";
 
 import AircraftEnum from "../../enums/Aircraft";
 import PageBanner from "../../components/PageBanner";
 import IconTextButton from "../../components/IconTextButton";
 import CloudImageBackground from "../../components/CloudImageBackground";
 
+import TutorialImage from "../../assets/tutorial1.png";
+
 const airplanes = [
   {
     title: "Passado",
     subtitle: "São situações que já \naconteceram",
     image: require("../../assets/aviao1.png"),
-    airplane: AircraftEnum.PAST
+    airplane: AircraftEnum.PAST,
   },
 
   {
-    title: "Presente", 
-    subtitle: "São situações que \nestão acontecendo", 
-    image: require("../../assets/aviao2.png"), 
-    airplane: AircraftEnum.PRESENT
+    title: "Presente",
+    subtitle: "São situações que \nestão acontecendo",
+    image: require("../../assets/aviao2.png"),
+    airplane: AircraftEnum.PRESENT,
   },
 
   {
-    title: "Futuro", 
-    subtitle: "São situações que \nainda irão acontecer", 
-    image: require("../../assets/aviao3.png"), 
-    airplane: AircraftEnum.FUTURE
-  }
+    title: "Futuro",
+    subtitle: "São situações que \nainda irão acontecer",
+    image: require("../../assets/aviao3.png"),
+    airplane: AircraftEnum.FUTURE,
+  },
 ];
 
 export default function Aircraft({ navigation }) {
   return (
     <CloudImageBackground>
-      <PageBanner title={'Vamos embarcar em qual avião?'} />
+      <PageBanner title={"Vamos embarcar em qual avião?"} />
       <View style={styles.container}>
         <View style={styles.content}>
-          {airplanes.map(ap => {
+          {airplanes.map((ap) => {
             const { title, subtitle, image, airplane } = ap;
 
             return (
-              <IconTextButton title={title}
+              <IconTextButton
+                title={title}
                 subtitle={subtitle}
                 navigation={navigation}
                 image={image}
-                onPress={_ => { navigation.navigate("TripBag", { aircraft: airplane })}} />
+                onPress={(_) => {
+                  navigation.navigate("TripBag", { aircraft: airplane });
+                }}
+              />
             );
           })}
         </View>
       </View>
+      <Modal
+        isVisible={true}
+        backdropOpacity={0.8}
+        animationIn="zoomInDown"
+        animationOut="zoomOutUp"
+        animationInTiming={600}
+        animationOutTiming={600}
+        backdropTransitionInTiming={600}
+        backdropTransitionOutTiming={600}
+      >
+        <Card>
+          <CardItem>
+            <Body style={{ alignItems: "center" }}>
+              <Image
+                source={TutorialImage}
+                style={{
+                  width: 200,
+                  height: 200,
+                  resizeMode: "contain",
+                }}
+              />
+              <View style={{ marginVertical: 40 }}>
+                <Text>
+                  Quando vamos viajar em nossos pensamentos, podemos escolher o
+                  tipo do avião.
+                </Text>
+                <Text>
+                  Avião do passado, pensamentos que já aconteceram e ficam em
+                  nossa cabeça.
+                </Text>
+                <Text>Avião do presente, pensamentos que são de agora.</Text>
+                <Text>
+                  E avião do futuro, pensamentos de situações que não
+                  acontecerão ainda...
+                </Text>
+              </View>
+              <Button
+                full
+                rounded
+                onPress={() => {
+                  alert("oi");
+                }}
+              >
+                <Text>Ok, entendi</Text>
+              </Button>
+            </Body>
+          </CardItem>
+        </Card>
+      </Modal>
     </CloudImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -60,5 +117,5 @@ const styles = StyleSheet.create({
   },
   content: {
     margin: 20,
-  }
+  },
 });
